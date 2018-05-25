295efe7f986319582ec7393723144da691e33176
soryrawyer <sory.rawyer@gmail.com>
2018-05-25 11:39:12 -0400

    add more back blazers
    Co-authored-by: AsHley Blewer <ashley.blewer@gmail.com>
    Co-authored-by: Ben "jort golem" Linsay <blinsay@gmail.com>

--- portal/b2.py
+++ portal/b2.py
@@ -15,8 +15,11 @@ class BackBlaze(object):
     def __init__(self):
         # non-standard way of storing/accessing b2 creds, I guess
         env_vars = os.environ
+        self.account_id = env_vars['B2_ACCOUNT_ID']
         self.auth_stuff = self.get_auth_token(env_vars['B2_ACCOUNT_ID'],
                                               env_vars['B2_APPLICATION_KEY'])
+        self._headers = {'Authorization' : self.auth_stuff['authorizationToken']}
+        self._params = {'accountId' : self.account_id}
 
     def get_auth_token(self, account_id, application_key):
         """
@@ -32,9 +35,32 @@ class BackBlaze(object):
             raise err
         return resp.json()
 
+    def list_buckets(self) -> list:
+        """
+        list_buckets : return a list of bucket information
+        """
+        url = os.path.join(self.auth_stuff['apiUrl'], 'b2api', 'v1', 'b2_list_buckets')
+        resp = requests.get(url, headers=self._headers, params=self._params)
+        resp.raise_for_status()
+        return resp.json()['buckets']
+
+    def get_file_upload_url(self, bucket_id) -> dict:
+        """
+        get_file_upload_url : pretty self-explanatory
+        """
+        url = os.path.join(self.auth_stuff['apiUrl'], 'b2api', 'v1', 'b2_get_upload_url')
+        params = {'bucketId' : bucket_id}
+        resp = requests.get(url, headers=self._headers, params=params)
+        resp.raise_for_status()
+        return resp.json()
+
 def main():
     b = BackBlaze()
-    print(b.auth_stuff)
+    # print(b.auth_stuff)
+    buckets = b.list_buckets()
+    print(buckets)
+    file_upload_spot = b.get_file_upload_url(buckets[0]['bucketId'])
+    print(file_upload_spot)
 
 if __name__ == '__main__':
     main()
