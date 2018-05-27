f3568137627fa1ef24b408a2e5c11470103baaee
soryrawyer <sory.rawyer@gmail.com>
2018-05-26 23:17:18 -0400

    update (or create) the index of stuff on cloud storage
    Co-authored-by: ashley blewer <ashley.blewer@gmail.com>

--- portal/ingest.py
+++ portal/ingest.py
@@ -14,7 +14,9 @@ look up artist/album/song using musicbrainz
 """
 
 import argparse
+import json
 import os
+import tempfile
 
 import musicbrainzngs as mb
 from mutagen import mp3, flac
@@ -36,7 +38,8 @@ def main(arguments):
     for track in enriched_metadata:
         _, ext = os.path.splitext(track['path'])
         ext = ext.replace('.', '')
-        upload_location = os.path.join(track['artist_id'],
+        upload_location = os.path.join(arguments.upload_prefix,
+                                       track['artist_id'],
                                        track['release_id'],
                                        '{}.{}'.format(track['track_id'], ext))
         print('Uploading {} to {}/{}'.format(track['path'],
@@ -45,6 +48,27 @@ def main(arguments):
         blob = bucket.blob(upload_location)
         with open(track['path'], 'rb') as audio:
             blob.upload_from_file(audio)
+    # tracks uploaded. now let's make that index
+    # first, check to see if it's already there
+    index_location = os.path.join('index', 'audio.json')
+    existing_blob = bucket.get_blob(index_location)
+    index = {}
+    for track in enriched_metadata:
+        index[track['track_id']] = track
+    if existing_blob:
+        # blob exists
+        beep = existing_blob.download_as_string()
+        existing_index = json.loads(beep)
+        # the next two lines are dumb and should be removed
+        existing_index.update(index)
+        index = existing_index
+    # the next 5 lines are also dumb and should also be removed
+    with open('audio_index.json', 'w') as tmp:
+        json.dump(index, tmp)
+    with open('audio_index.json', 'rb') as tmp:
+        blob = bucket.blob(index_location)
+        blob.upload_from_file(tmp)
+
 
 def get_musicbrainz_data():
     """
@@ -135,5 +159,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('music_location')
     parser.add_argument('upload_bucket')
+    parser.add_argument('upload_prefix')
     args = parser.parse_args()
     main(args)
